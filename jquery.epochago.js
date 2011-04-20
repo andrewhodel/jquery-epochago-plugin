399cb02ee8428570576d99a20718f14086da1f35
Andrew Hodel <andrewhodel@gmail.com>
2011-04-20 11:15:00 -0700

    modified:   jquery.epochago.js

--- jquery.epochago.js
+++ jquery.epochago.js
@@ -12,11 +12,36 @@ $.fn.epochago = function(options) {
 		obj = $(this);
 		var body = obj.html();
 
-		// get time since in seconds
 		var ts = Math.round((new Date()).getTime() / 1000);
-		var diff = ts - body;
+		var seconds = ts - body;
+		var minutes = seconds / 60;
+		var hours = minutes / 60;
+		var days = hours / 24;
+		var years = days / 365;
 
-		alert(diff+' sec ago');
+		var str = '';
+
+		function pStr(count, period) {
+			if (count > 1) {
+				return 'about '+count+' '+period+'s ago';
+			} else {
+				return 'about '+count+' '+period+' ago';
+			}
+		}
+
+		if (years >= 1) {
+			str = pStr(years, 'year');
+		} else if (days >= 1) {
+			str = pStr(days, 'day');
+		} else if (hours >= 1) {
+			str = pStr(hours, 'hour');
+		} else if (minutes >= 1) {
+			str = pStr(minutes, 'minute');
+		} else {
+			str = pStr(seconds, 'second');
+		}
+
+		alert(str);
 
 	});
 };
